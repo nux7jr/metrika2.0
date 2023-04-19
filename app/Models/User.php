81898d835df01f70a4f9aad807b1cfa31673693f
Denis Zarubin <Denis13216@gmail.com>
2023-04-19 14:37:21 +0700

    Revert "adding privilegies"
    This reverts commit 32dbf7b8f27bf8763a5e027d59d121a9aaae1b7a.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Wnikk\LaravelAccessRules\Traits\HasPermissions;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable,HasPermissions;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
