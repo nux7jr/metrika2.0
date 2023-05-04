141f50008ed8de568ab65163e504d969239d1650
Denis Zarubin <Denis13216@gmail.com>
2023-05-04 09:45:21 +0700

    add xl-pipe/daewoo

--- app/Http/Helpers/Reports/DailyReport.php
+++ app/Http/Helpers/Reports/DailyReport.php
@@ -25,14 +25,14 @@ class DailyReport
     public function __construct(string $date)
     {
         $this->settings['date'] = $date;
-        $this->date = date('d.m.y', strtotime($date));
+        $this->date = date('d.m.y',strtotime($date));
         $leads = new GetLeads($this->path);
-        $temp_leads = $leads->get_leads_by_date($date, $date);
-        if (empty($temp_leads)) {
+        $temp_leads = $leads->get_leads_by_date($date,$date);
+        if (empty($temp_leads)){
             throw new \Exception('В файле с лидами нет данных');
         }
-        if (empty($temp_leads[$this->date])) {
-            throw new \Exception('В файле с лидами нет данных на текущую дату: ' . $this->date);
+        if (empty($temp_leads[$this->date])){
+            throw new \Exception('В файле с лидами нет данных на текущую дату: '.$this->date);
         }
         $this->leads = $temp_leads[$this->date];
         unset($temp_leads);
@@ -44,16 +44,16 @@ class DailyReport
     {
         $result = $this->countLeads->prepare_summary_report();
         $result['Малые этажи'] = $this->bitrixAPI->getDataFromEtaji();
-        $this->bitrixAPI->setDirection(94); //вопронка СТП Развитие
+        $this->bitrixAPI->setDirection(94);//вопронка СТП Развитие
         $result['Диллерство полы'][] = $this->bitrixAPI->getDataFromSTP();
-        $this->bitrixAPI->setDirection(92); //Воронка NF Развитие
+        $this->bitrixAPI->setDirection(92);//Воронка NF Развитие
         $result['Нанофайбер франшиза СНГ'][] = $this->bitrixAPI->getDataFromNFSNG();
-        $this->bitrixAPI->setDirection(112); //Воронка NF World
+        $this->bitrixAPI->setDirection(112);//Воронка NF World
         $result['Нанофайбер франшиза Зарубежные страны'][] = $this->bitrixAPI->getDataFromNFWorld();
 
         $result_count = $this->getResultCount($result);
         $noutm_report = $this->countLeads->prepare_summary_report($noutm = true);
-        $this->bitrixAPI->setDirection(46); //Воронка Продаж
+        $this->bitrixAPI->setDirection(46);//Воронка Продаж
         $noutm_report['Малые этажи'] = $this->bitrixAPI->getDataFromEtajiNoUtm();
         $noutm_report['Диллерство полы'] = $result['Диллерство полы'];
         $noutm_report['Нанофайбер франшиза СНГ'] = $result['Нанофайбер франшиза СНГ'];
@@ -62,7 +62,7 @@ class DailyReport
 
         //        $resultJSON['header'] = $this->getHeaders();
         $resultJSON[0] = array_merge(['state' => 'Рекламные'], $result_count);
-        $resultJSON[1] = array_merge(['state' => 'Не рекламные'], $result_count_no_utm);
+        $resultJSON[1] = array_merge(['state' => 'Все'], $result_count_no_utm);
 
         return json_encode($resultJSON);
     }
@@ -70,33 +70,39 @@ class DailyReport
     private function getResultCount(array $report): array
     {
         return $result_count = [
-            'krsk_foolrs' => count($report['Теплые полы']['Красноярск'] ?? []),
-            'msk_foolrs' => count($report['Теплые полы']['Москва'] ?? []),
-            'dealers_foolrs' => count($report['Теплые полы']['Диллеры'] ?? []),
-            'krsk_boilers' => count($report['Котлы']['Красноярск'] ?? []),
-            'krsk_promboilers' => count($report['Промкотлы']['Красноярск'] ?? []),
-            'msk_boilers' => count($report['Котлы']['Москва'] ?? []),
-            'dealers_franchisees' => count($report['Диллерство полы'][0] ?? []),
-            'nanofiber_franchisees_sng' => count($report['Нанофайбер франшиза СНГ'][0] ?? []),
-            'nanofiber_franchisees_world' => count($report['Нанофайбер франшиза Зарубежные страны'][0] ?? []),
-            'krsk_etaji' => count($report['Малые этажи']['Красноярск'] ?? []),
-            'dealers_etaji' => (count($report['Малые этажи']['Тюмень'] ?? []) + count($report['Малые этажи']['Иркутск'] ?? []) + count($report['Малые этажи']['Владивосток'] ?? []) + count($report['Малые этажи']['Пермь'] ?? []) + count($report['Малые этажи']['Екатеринбург'] ?? [])),
-            'tumen_etaji' => count($report['Малые этажи']['Тюмень'] ?? []),
-            'irkutsk_etaji' => count($report['Малые этажи']['Иркутск'] ?? []),
-            'vladivostok_etaji' => count($report['Малые этажи']['Владивосток'] ?? []),
-            'perm_etaji' => count($report['Малые этажи']['Пермь'] ?? []),
-            'ekb_etaji' => count($report['Малые этажи']['Екатеринбург'] ?? []),
-            'barnaul_etaji' => count($report['Малые этажи']['Барнаул'] ?? []),
-            'tiksan_auto' => count($report['tiksan_auto'][0] ?? [])
-        ];
+                    'krsk_foolrs_xl_pipe' => count($report['Теплые полы']['xl-pipe']['Красноярск'] ?? []),
+                    'krsk_foolrs_daewoo' => count($report['Теплые полы']['daewoo']['Красноярск'] ?? []),
+                    'msk_foolrs_xl_pipe' => count($report['Теплые полы']['xl-pipe']['Москва'] ?? []),
+                    'msk_foolrs_daewoo' => count($report['Теплые полы']['daewoo']['Москва'] ?? []),
+                    'dealers_foolrs_xl_pipe' => count($report['Теплые полы']['xl-pipe']['Диллеры'] ?? []),
+                    'dealers_foolrs_daewoo' => count($report['Теплые полы']['daewoo']['Диллеры'] ?? []),
+                    'krsk_boilers' => count($report['Котлы']['Красноярск'] ?? []),
+                    'krsk_promboilers' => count($report['Промкотлы']['Красноярск'] ?? []),
+                    'msk_boilers' => count($report['Котлы']['Москва'] ?? []),
+                    'dealers_franchisees' => count($report['Диллерство полы'][0] ?? []),
+                    'nanofiber_franchisees_sng' => count($report['Нанофайбер франшиза СНГ'][0] ?? []),
+                    'nanofiber_franchisees_world' => count($report['Нанофайбер франшиза Зарубежные страны'][0] ?? []),
+                    'krsk_etaji' => count($report['Малые этажи']['Красноярск'] ?? []),
+                    'dealers_etaji' => (count($report['Малые этажи']['Тюмень'] ?? []) + count($report['Малые этажи']['Иркутск'] ?? []) + count($report['Малые этажи']['Владивосток'] ?? []) + count($report['Малые этажи']['Пермь'] ?? []) + count($report['Малые этажи']['Екатеринбург'] ?? [])),
+                    'tumen_etaji' => count($report['Малые этажи']['Тюмень'] ?? []),
+                    'irkutsk_etaji' => count($report['Малые этажи']['Иркутск'] ?? []),
+                    'vladivostok_etaji' => count($report['Малые этажи']['Владивосток'] ?? []),
+                    'perm_etaji' => count($report['Малые этажи']['Пермь'] ?? []),
+                    'ekb_etaji' => count($report['Малые этажи']['Екатеринбург'] ?? []),
+                    'barnaul_etaji' => count($report['Малые этажи']['Барнаул'] ?? []),
+                    'tiksan_auto' => count($report['tiksan_auto'][0] ?? [])
+                ];
     }
 
     private function getHeaders(): array
     {
         return $result_header = [
-            'krsk_foolrs' => 'Красноярск ТП',
-            'msk_foolrs' => 'Москва ТП',
-            'dealers_foolrs' => 'Дилеры ТП',
+            'krsk_foolrs_xl_pipe' => 'Красноярск ТП xl-pipe',
+            'krsk_foolrs_daewoo' => 'Красноярск ТП daewoo',
+            'msk_foolrs_xl_pipe' => 'Москва ТП xl-pipe',
+            'msk_foolrs_daewoo' => 'Москва ТП daewoo',
+            'dealers_foolrs_xl_pipe' => 'Дилеры ТП xl-pipe',
+            'dealers_foolrs_daewoo' => 'Дилеры ТП daewoo',
             'krsk_boilers' => 'Красноярск Котлы',
             'krsk_promboilers' => 'Красноярск промкотлы',
             'msk_boilers' => 'Москва Котлы',
