7f5368d776d7a18620b431614631e3ba55bc6a05
Denis Zarubin <Denis13216@gmail.com>
2023-05-30 14:53:50 +0700

    test deals updater from B24 webhook

--- app/Http/Controllers/DealsBitrixController.php
+++ app/Http/Controllers/DealsBitrixController.php
@@ -12,6 +12,18 @@ use Illuminate\Validation\Validator;
 class DealsBitrixController extends Controller
 {
     /**
+     * @var Client
+     */
+    private static Client $guzzle;
+    public function __construct(){
+        self::$guzzle = new Client(
+            config: [
+                'base_uri' => 'https://tiksan-group.bitrix24.ru/rest/1/t7wy1lx66dizck10/',
+                'timeout'  => '10.0',
+            ]
+        );
+    }
+    /**
      * @param Request $request
      * @return void
      * @throws GuzzleException
@@ -28,8 +40,15 @@ class DealsBitrixController extends Controller
                 'event' => 'required|string',
             ]
         );
-        $this->sendTelegram(serialize($validated));
+        $deal_info = $this->getDealById(intval($validated['data.FIELDS.ID']));
+        $this->sendTelegram(serialize($deal_info));
     }
+
+    /**
+     * check input webhook token
+     * @param string $request_token
+     * @return bool
+     */
     private static function authorizeRequest(string $request_token): bool{
         $tokens = [
             'gu04g0rumcbps79d7vhxwbii0pywbidf' => true,
@@ -41,15 +60,36 @@ class DealsBitrixController extends Controller
         return false;
     }
 
-    private function sendTelegram(string $mess, int $chat_id = 233617089){
-        $guzzle = new Client(
-            config: [
-                'base_uri' => 'https://api.telegram.org/bot5970971353:AAHyyXHLAMyC86oIiUREWrMNk8baHoYYZ4E/',
-                'timeout'  => '10.0',
-            ]
+    private function getDealById(int $id): array{
+        $response = self::$guzzle->post(
+            uri:'crm.deal.list.json',
+            options: [
+                'method'    => 'POST',
+                'body'      => json_encode([
+                    'order'     => ['DATE_CREATE'=>'ASC'],
+                    'filter'    => ['ID'=>$id],
+                    'select'    => [
+                        'BEGINDATE',
+                        'STAGE_ID',
+                        'ID',
+                        'CLOSEDATE',
+                        'DATE_CREATE',
+                        'DATE_MODIFY',
+                        'CLOSED',
+                        'CONTACT_ID',
+                        'CATEGORY_ID',
+                        'CURRENCY_ID',
+                        'OPPORTUNITY',
+                    ],
+                ]),
+            ],
         );
-        $response = $guzzle->post(
-            uri:'sendMessage',
+        return json_decode($response->getBody());
+    }
+
+    private function sendTelegram(string $mess, int $chat_id = 233617089){
+        $response = self::$guzzle->post(
+            uri:'https://api.telegram.org/bot5970971353:AAHyyXHLAMyC86oIiUREWrMNk8baHoYYZ4E/sendMessage',
             options: [
                 'method'    => 'POST',
                 'query'      => [
