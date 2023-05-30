2d4b17e8d224bb1563feca6ae2b13ad4f9f7683f
Denis Zarubin <Denis13216@gmail.com>
2023-05-30 18:06:41 +0700

    add DealsBitrixController.php (need fixed declaration Collection)

--- app/Http/Controllers/DealsBitrixController.php
+++ app/Http/Controllers/DealsBitrixController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\PostgresConnection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Validation\Validator;
+use Throwable;
 
 class DealsBitrixController extends Controller
 {
@@ -28,7 +31,7 @@ class DealsBitrixController extends Controller
      * @return void
      * @throws GuzzleException
      */
-    public function updateDeal(Request $request){
+    public function update(Request $request){
         $input = $request->toArray();
         if (!self::authorizeRequest($input['auth']['application_token'])){
             Log::info('Запрос с неизвестного приложения. input: ' . json_encode($input));
@@ -40,8 +43,19 @@ class DealsBitrixController extends Controller
                 'event' => 'required|string',
             ]
         );
-        $deal_info = $this->getDealById(intval($validated['data.FIELDS.ID']));
-        $this->sendTelegram(serialize($deal_info));
+        $deal_info = $this->getDealById(intval($validated['data']['FIELDS']['ID']));
+        if (empty($deal_info['result'])){
+            Log::info('Пришел пустой ответ от Битрикс24. input: ' . json_encode($input));
+            return 'result is empty';
+        }
+        $connection = DB::connection('pgsql');
+        $deal = $connection->table('deals')->where('id','=',$validated['data']['FIELDS']['ID']);
+        if (!$deal->exists()){
+            $this->insertDeal($deal_info['result'][0], $connection);
+            return 'deal was created';
+        }
+        $this->updateDeal($deal_info['result'][0], $connection, $deal);
+        return 'deal was updated';
     }
 
     /**
@@ -60,12 +74,18 @@ class DealsBitrixController extends Controller
         return false;
     }
 
+    /**
+     * get array deal from Bitrix
+     * @param int $id
+     * @return array
+     * @throws GuzzleException
+     */
     private function getDealById(int $id): array{
         $response = self::$guzzle->post(
             uri:'crm.deal.list.json',
             options: [
                 'method'    => 'POST',
-                'body'      => json_encode([
+                'query'      => ([
                     'order'     => ['DATE_CREATE'=>'ASC'],
                     'filter'    => ['ID'=>$id],
                     'select'    => [
@@ -80,13 +100,144 @@ class DealsBitrixController extends Controller
                         'CATEGORY_ID',
                         'CURRENCY_ID',
                         'OPPORTUNITY',
+                        'UTM_SOURCE',
+                        'UTM_MEDIUM',
+                        'UTM_CAMPAIGN',
+                        'UTM_TERM',
+                        'UTM_CONTENT',
+                        'UF_CRM_1681873184', // поле url
                     ],
                 ]),
             ],
         );
-        return json_decode($response->getBody());
+        return json_decode($response->getBody()->getContents(), true);
     }
 
+    /**
+     * get Phone from Bitrix
+     * @param int $id
+     * @return array
+     * @throws GuzzleException
+     */
+    private function getContactById(int $id): array{
+        $response = self::$guzzle->post(
+            uri:'crm.contact.list.json',
+            options: [
+                'method'    => 'POST',
+                'query'      => ([
+                    'order'     => ['DATE_CREATE'=>'ASC'],
+                    'filter'    => ['ID'=>$id],
+                    'select'    => [
+                        'PHONE',
+                    ],
+                ]),
+            ],
+        );
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * @param array $deal
+     * @param PostgresConnection $connection
+     * @return void
+     * @throws GuzzleException
+     * @throws Throwable
+     */
+    public function insertDeal(array $deal, PostgresConnection $connection): void{
+        try {
+            $contact = $this->getContactById(intval($deal['CONTACT_ID']));
+
+            $is_adv = false;
+            !empty($deal['UTM_SOURCE'])     ? $deal['UTM_SOURCE'] !== 'TEST' ? $is_adv = true : '' : '';
+            !empty($deal['UTM_MEDIUM'])     ? $deal['UTM_MEDIUM'] !== 'TEST' ? $is_adv = true : '' : '';
+            !empty($deal['UTM_CAMPAIGN'])   ? $is_adv = true : '';
+            !empty($deal['UTM_TERM'])       ? $is_adv = true : '';
+            !empty($deal['UTM_CONTENT'])    ? $is_adv = true : '';
+
+            $carbon = new Carbon($deal['DATE_CREATE']);
+            $carbon->setTimezone(7);
+            $date_create = $carbon->toDateTimeString();
+            $carbon->setTimezone(3);
+            $carbon->setDateTimeFrom($deal['DATE_MODIFY']);
+            $carbon->setTimezone(7);
+            $date_updated = $carbon->toDateTimeString();
+
+            $phone = phoneFormatter($contact['result'][0]['PHONE'][0]['VALUE']);
+
+            $insert_data = [
+                'id'            => $deal['ID'],
+                'is_adv'        => $is_adv,
+                'utm_source'    => $deal['UTM_SOURCE'],
+                'utm_medium'    => $deal['UTM_MEDIUM'],
+                'utm_campaign'  => $deal['UTM_CAMPAIGN'],
+                'utm_content'   => $deal['UTM_CONTENT'],
+                'utm_term'      => $deal['UTM_TERM'],
+                'url'           => $deal['UF_CRM_1681873184'],
+                'stage_now'     => $deal['STAGE_ID'],
+                'income'        => floatval($deal['OPPORTUNITY']),
+                'currency'      => $deal['CURRENCY_ID'],
+                'phone'         => $phone,
+                'created_at'    => $date_create,
+                'updated_at'    => $date_updated,
+            ];
+            Log::info('insert Data: ' . json_encode($insert_data));
+            $connection->beginTransaction();
+            $result = $connection->table('deals')->insert($insert_data);
+            Log::info('deal was created.');
+            $connection->commit();
+        }catch (\Exception $error){
+            $connection->rollBack();
+            $this->sendTelegram('error create Deal. Message: ' . $error->getMessage());
+            Log::info('error create Deal. Message: ' . $error->getMessage());
+        }
+
+    }
+
+    /**
+     * @param array $deal
+     * @param PostgresConnection $connection
+     * @param Collection $db_deal
+     * @return void
+     * @throws GuzzleException
+     * @throws Throwable
+     */
+    public function updateDeal(array $deal, PostgresConnection $connection, Collection $db_deal){
+        try {
+            $carbon = new Carbon($deal['DATE_MODIFY']);
+            $carbon->setTimezone(7);
+            $date_updated = $carbon->toDateTimeString();
+
+            $data_updated = [
+                'updated_at' => $date_updated,
+            ];
+            $db_deal->updated_at = $date_updated;
+            $db_deal->currency !== $deal['CURRENCY_ID'] ? $data_updated['currency'] = $deal['CURRENCY_ID'] : '';
+            $db_deal->income !== floatval($deal['OPPORTUNITY']) ? $data_updated['income'] = floatval($deal['OPPORTUNITY']) : '';
+            if($db_deal->stage_now !== $deal['STAGE_ID']){
+                $stages = json_decode($db_deal->stage_changes, true);
+                $datetime = Carbon::today()->toDateTimeString();
+                $stages[] = "{\"from\":\"{$db_deal->stage_now}\",\"to\":\"{$deal['STAGE_ID']}\",\"datetime\":\"{$datetime}\"}";
+                $data_updated['stage_changes'] = $stages;
+                $data_updated['stage_now'] = $deal['STAGE_ID'];
+            }
+
+            Log::info('update Data: ' . json_encode($data_updated));
+            $connection->beginTransaction();
+            $db_deal->update($data_updated);
+            Log::info('deal was updated.');
+            $connection->commit();
+        }catch (\Exception $error){
+            $connection->rollBack();
+            $this->sendTelegram('error update Deal. Message: ' . $error->getMessage());
+            Log::info('error update Deal. Message: ' . $error->getMessage());
+        }
+    }
+    /**
+     * @param string $mess
+     * @param int $chat_id
+     * @return void
+     * @throws GuzzleException
+     */
     private function sendTelegram(string $mess, int $chat_id = 233617089){
         $response = self::$guzzle->post(
             uri:'https://api.telegram.org/bot5970971353:AAHyyXHLAMyC86oIiUREWrMNk8baHoYYZ4E/sendMessage',
