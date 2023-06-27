4b7cf2939bd9492712d526c76223278c26599374
Yakudza228 <st2222s@mail.ru>
2023-06-27 17:52:19 +0700

    update_favicon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,7 +14,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
-
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
